a2b833c4d3df1043c9e791eea3f4e1e39997c9fc
Jonathan Porta <jonathan@jonathanporta.com>
2020-05-20 16:05:26 -0600

    re-add conda install

--- installation/python3.sh
+++ installation/python3.sh
@@ -15,13 +15,13 @@ else
 fi
 
 
-# if ! (exists anaconda); then
-#   echo "Installing anaconda....Comment this out if you don't want none, honey."
-#   # # https://docs.anaconda.com/anaconda/install/linux/
-#   # sudo dnf install -y libXcomposite libXcursor libXi libXtst libXrandr alsa-lib mesa-libEGL libXdamage mesa-libGL libXScrnSaver
-#   # curl -Lo anaconda.sh https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
-#   # chmod +x ./anaconda.sh
-#   # ./anaconda.sh && rm ./anaconda.sh
-# else
+if ! (exists anaconda); then
+  echo "Installing anaconda....Comment this out if you don't want none, honey."
+  # # https://docs.anaconda.com/anaconda/install/linux/
+  sudo dnf install -y libXcomposite libXcursor libXi libXtst libXrandr alsa-lib mesa-libEGL libXdamage mesa-libGL libXScrnSaver
+  curl -Lo $INSTALL_DIR/anaconda.sh https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+  chmod +x $INSTALL_DIR/anaconda.sh
+  $INSTALL_DIR/anaconda.sh && rm $INSTALL_DIR/anaconda.sh
+else
   echo "anaconda is already installed...skipping"
-# fi
+fi
