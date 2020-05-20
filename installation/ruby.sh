ad74aa6fc29e88a4b51c2b03e9fb868ad865b890
Jonathan Porta <jonathan@jonathanporta.com>
2020-05-20 17:00:59 -0600

    Specify extraction location

--- installation/ruby.sh
+++ installation/ruby.sh
@@ -6,7 +6,7 @@ set -e
 
 if ! (exists chruby); then
   wget -O $INSTALL_DIR/chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
-  tar -xzvf $INSTALL_DIR/chruby-0.3.9.tar.gz
+  tar -xzvf $INSTALL_DIR/chruby-0.3.9.tar.gz -C $INSTALL_DIR/
   cd $INSTALL_DIR/chruby-0.3.9/
   sudo make install && cd - && rm $INSTALL_DIR/chruby-0.3.9.tar.gz && rm -rf $INSTALL_DIR/chruby-0.3.9
 fi
@@ -14,7 +14,7 @@ fi
 # ruby-install
 if ! (exists ruby-install); then
   wget -O $INSTALL_DIR/ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz
-  tar -xzvf $INSTALL_DIR/ruby-install-0.7.0.tar.gz
+  tar -xzvf $INSTALL_DIR/ruby-install-0.7.0.tar.gz -C $INSTALL_DIR/
   cd $INSTALL_DIR/ruby-install-0.7.0/
   sudo make install && cd - && rm $INSTALL_DIR/ruby-install-0.7.0.tar.gz && rm -rf $INSTALL_DIR/ruby-install-0.7.0
 fi
