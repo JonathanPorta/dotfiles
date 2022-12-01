aa7625ea543120521bbc7bf984e53c28a115ba2c
Jonathan Porta <jonathan@jonathanporta.com>
2022-12-01 03:37:27 -0700

    Add firstrun permissioning

--- hello-world.sh
+++ hello-world.sh
@@ -1,18 +1,54 @@
 #!/bin/bash
 set -e
 
-GH_USERNAME='JonathanPorta'
+LOCAL_USERNAME=portaj
+GH_USERNAME=JonathanPorta
+HOME_DIR=/home/${LOCAL_USERNAME}
+SSH_DIR=${HOME_DIR}/.ssh
+
+echo "Setting up this machine using:"
+echo "  Local Username: ${LOCAL_USERNAME}"
+echo "  Github Username: ${GH_USERNAME}"
+echo "  Home Directory: ${HOME_DIR}"
+echo "  SSH Directory: ${SSH_DIR}"
+
+
+echo "Ensure .ssh directory exists, is owned by $LOCAL_USERNAME, and has correct permissions"
+mkdir -p ${SSH_DIR}
+chown -R ${LOCAL_USERNAME} ${SSH_DIR}
+chmod 700 ${SSH_DIR}/
+
+ls -lah ${SSH_DIR}
+echo "Done."
+
+if test -f "${SSH_DIR}/id_rsa"
+then
+    echo "${SSH_DIR}/id_rsa exists. Not regenerating."
+else
+    echo "${SSH_DIR}/id_rsa doesn't exist. Generating new SSH key..."
+    ssh-keygen -b 4096 -t rsa-sha2-512 -f ${SSH_DIR}/id_rsa -q -N ""
+    echo "Done."
+fi
+
 
 echo "Ensure GitHub keys are synced to local authorized_keys..."
 # we want to be able to run this and update the authorized keys with whatever we have on GH
-curl https://api.github.com/users/${GH_USERNAME}/keys | jq -r '.[] | .key' > $HOME/.ssh/authorized_keys
-echo "Updated '$HOME/.ssh/authorized_keys' to:"
-cat $HOME/.ssh/authorized_keys
-echo "Truncated and wrote $(cat $HOME/.ssh/authorized_keys | wc -l) keys to '$HOME/.ssh/authorized_keys'."
+curl https://api.github.com/users/${GH_USERNAME}/keys | jq -r '.[] | .key' > ${SSH_DIR}/authorized_keys
+echo "Updated '${SSH_DIR}/authorized_keys' to:"
+cat ${SSH_DIR}/authorized_keys
+echo "Truncated and wrote $(cat ${SSH_DIR}/authorized_keys | wc -l) keys to '${SSH_DIR}/authorized_keys'."
 echo "Done."
 
 echo "Ensure sshd is enabled and running..."
 sudo systemctl enable sshd
 sudo systemctl start sshd
 sudo systemctl status sshd
+echo "Done."
+
+echo "Ensure .ssh files have the correct permissions..."
+chmod 640 ${SSH_DIR}/authorized_keys
+chmod 644 ${SSH_DIR}/*.pub
+chmod 600 ${SSH_DIR}/id_rsa
+
+ls -lah ${SSH_DIR}
 echo "Done."
